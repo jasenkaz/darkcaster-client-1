7442d6f82a9c31d90eee6fb9f36226555cc382db
Justin Dennison <justin1dennison@gmail.com>
2017-07-06 18:49:15 -0400

    added build script and refactored to use getter syntax in AngularJS as well as remove the need for $scope

--- src/app.js
+++ src/app.js
@@ -1,13 +1,14 @@
 const angular = require('angular');
 
 //create our application
-const app = angular.module('darkcaster-client', []); //setter syntax
+angular.module('darkcaster-client', []); //setter syntax
 
-app.controller('MainController', MainController);
+angular.module('darkcaster-client') //getter
+       .controller('MainController', MainController);
 
-MainController.$inject = ['$scope']; //magic for us
+MainController.$inject = []; //magic for us
 
-function MainController($scope){
-  $scope.message = 'hello from angular';
-  
+function MainController(){
+  this.message = 'hello from angular';
+
 }
