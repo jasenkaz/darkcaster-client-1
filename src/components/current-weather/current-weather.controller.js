e57ed17f0b480037eab79352ecb66376fb3f25ed
Justin Dennison <justin1dennison@gmail.com>
2017-07-12 21:58:52 -0400

    added the ability for a user to search using inputs and a button based search

--- src/components/current-weather/current-weather.controller.js
+++ src/components/current-weather/current-weather.controller.js
@@ -3,14 +3,19 @@ const clearDay = require('../../images/clear-day.png');
 MainController.$inject = ['WeatherService']; //magic for us
 
 function MainController(weather){
-  this.message = 'hello from angular';
+  this.lat = 0;
+  this.lon = 0;
   this.imageLookup = {
     'clear-day': clearDay
   };
-
-  weather.getCurrentWeather(29, -81)
-         .then(currentWeather => this.weatherData = currentWeather); //is a promise
-
+  //functions
+  this.search = function search(){
+    console.log('searching....');
+    console.log(this.lat);
+    console.log(this.lon);
+    weather.getCurrentWeather(this.lat, this.lon)
+           .then(currentWeather => this.weatherData = currentWeather); //is a promise
+  };
 
 }
 module.exports = MainController;
