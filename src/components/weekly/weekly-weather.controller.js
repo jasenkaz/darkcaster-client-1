a607ffc8d498ee635def8052e53773daf54615a5
jasenkaz <jasenkaz@yahoo.com>
2017-07-26 13:46:34 -0400

    weekly components updated

--- src/components/weekly/weekly-weather.controller.js
+++ src/components/weekly/weekly-weather.controller.js
@@ -1,17 +1,17 @@
-const clearDay = require('../../images/clear-day.png');
 
-WeeklyWeatherController.$inject = ['WeatherService']; //magic for us
 
-function WeeklyWeatherController(weather){
+WeeklyWeatherController.$inject = ['WeatherService', 'images']; //magic for us
+
+function WeeklyWeatherController(weather, images){
   this.lat = 0;
   this.lon = 0;
-  this.imageLookup = {
-    'clear-day': clearDay
-  };
+  this.imageLookup = images.lookup;
+  this.missingImage = images.missing;
   //functions
-  this.search = function search(){
+  this.search = search;
+  function search(){
     weather.getWeeklyWeather(this.lat, this.lon)
-           .then(weeklyWeather => this.weatherData = weeklyWeather); //is a promise
+           .then(weeklyWeather => this.weeklyWeather = weeklyWeather); //is a promise
   };
 
 }
